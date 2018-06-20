e9964c8cb10d993c60fbd739ee1d6a9d4f03f61e
Eshipillah Pasili Samson <samesheshipilla94@gmail.com>
2018-06-20 18:26:55 -0400

    First Commit

---
+++ src/main/java/com/krib/controllers/KribPagesController.java
@@ -0,0 +1,25 @@
+/*
+ * This is a contoller class for all the web pages used
+ * used by Krib. It has methods that map requested pages
+ * to the resources hosted on the server 
+ */
+package com.krib.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Eshipillah Pasili Samson
+ */
+@Controller
+public class KribPagesController {
+    // this is the page suffix to be used by the user when sending a 
+    // request e.g home.hmtl, home.php (meh :-(, haha), home.asp
+    private String pageSuffix = ".html"; 
+    
+    @GetMapping
+    public String tenantDashboard(){
+        return "user_dashboard"; 
+    }
+}
---
+++ src/main/java/com/krib/controllers/KribRestController.java
@@ -0,0 +1,15 @@
+/*
+ * This is a controller for all the REST API endpoints 
+ */
+package com.krib.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Eshipillah Pasili Samson 
+ */
+@RestController
+public class KribRestController {
+    
+}
